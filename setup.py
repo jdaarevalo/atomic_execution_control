5370e11e8cb33facdc2555eaca6309c631f4073e
David Arevalo <david.arevalo@latana.com>
2024-04-02 15:12:54 +0200

    new version deployed

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 from setuptools import setup, find_packages
 
+# Dynamic loading of the long description
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='atomic_execution_control',
-    version='0.1.3',
+    version='0.1.5',
     author='Jose David Arevalo',
     author_email='jdaarevalo@gmail.com',
-    description='A utility for atomic DynamoDB operations in AWS Lambda, Fargate, EC2 ... functions',
-    long_description=open('README.md').read(),
+    description='Ensures atomic executions across AWS services using DynamoDB to prevent race conditions in distributed applications.',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/jdaarevalo/atomic_execution_control',
     packages=find_packages(),
     install_requires=[
-        'boto3'
+        'boto3>=1.14.0'
     ],
+    python_requires='>=3.6',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    keywords='atomic execution, DynamoDB, AWS Lambda, AWS Fargate, EC2, distributed systems, concurrency control, race condition prevention, cloud computing, AWS services, state management',
 )
